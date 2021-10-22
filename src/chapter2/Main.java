34af08432299130cbd72c9382778acd0d45c1ab6
tom803 <tom803@users.noreply.github.com>
2021-10-22 14:11:43 +0200

    change geneStr1 in Main.java

--- src/chapter2/Main.java
+++ src/chapter2/Main.java
@@ -3,7 +3,7 @@ package chapter2;
 public class Main {
 
 	public static void main(String[] args) {
-		String geneStr1 = "ACGTGGCTCTCTAACGTACGTA";
+		String geneStr1 = "ACGTGGCTCTCTAACGTACGTACGTGGCTCTCTAACGTACGTA";
 		
 		Gene gen1 = new Gene(geneStr1);
 		
